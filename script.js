d63167cdca5370f87efad42a13834dd3b618294b
Daniel Rodrigo Beltrame <93795810+DanielRodrigoBeltrame@users.noreply.github.com>
2021-11-24 19:37:42 -0300

    Alteração JS

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 
+// apontamento para os elementos html do DOM que vou usar
 const result=document.getElementById("result")
 
-let filmes = ["Eternos", "Homem Aranha", "Thor", "Titanic", "De volta para o Futuro", "Matrix", "ET", "O poderoso Chefão", "As Branquelas", "Star Wars", "Jurassic Park", "George O Rei da Floresta", "Harry Potter"];
 
-let desenhos = ["Os Simpsons", "Caverna do Dragão", "Tom e Jerry", "Bob Esponja", "Os Padrinhos Mágicos", ]
+const filmes = ["Eternos", "Homem Aranha", "Thor", "Titanic", "De volta para o Futuro", "Matrix", "ET", "O poderoso Chefão", "As Branquelas", "Star Wars", "Jurassic Park", "George O Rei da Floresta", "Harry Potter"];
 
+let filme = null
+
+const desenhos = ["Os Simpsons", "Caverna do Dragão", "Tom e Jerry", "Bob Esponja", "Os Padrinhos Mágicos", ]
+
+// alteradores de estado da aplicação
 function sortear() {
+    filme = filmes[Math.floor(Math.random()*filmes.length)];
     
-    let filmesrandom = filmes[Math.floor(Math.random()*filmes.length)];
-    result.innerText = filmesrandom;
+    render()
+
+}
+
+// mostra o estado atual da aplicação na tela
+
+function render(){
+    result.innerText = filme
 
 }
